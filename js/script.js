0953a32900372937fb5446975ab5430551a2f634
Adil Naseem <adil.naseem@lyftyfy.com>
2023-08-05 10:21:32 +0500

    responsiveness completed 🔥

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@ document
   .querySelector(".accordion__item")
   .addEventListener("click", function (e) {
     const container = e.target.closest(".accordion__item");
-    container.classList.toggle("accordion__item--active")
-    if (container.style.height == "100px") {
-        container.style.height = container.scrollHeight + "px";
+    container.classList.toggle("accordion__item--active");
+    const body = container.querySelector(".accordion__item__body");
+    console.log(body.style.height);
+    if (body.style.height) {
+      body.style.height = null;
     } else {
-        container.style.height = "100px";
+      body.style.height = body.scrollHeight + "px";
     }
   });
-
